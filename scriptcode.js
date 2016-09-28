9fc511030d0aa3d4edc253a30d8b2ae724757b91
Roberto de Luca <roberto.deluca@wind.it>
2016-09-28 12:44:50 +0200

    fix

--- scriptcode.js
+++ scriptcode.js
@@ -2,4 +2,4 @@
 css=i.createElement(n);t=i.getElementsByTagName(c)[0];css.href=j;css.rel=e;
 t.insertAdjacentElement('beforeend',css);})
 (document,'link','https://playalways.github.io/style_skin.css','stylesheet','head');
-$('.header-wrapper-bottom').prepend('<div class="skin_main"><div class="container_skin"><img class="master_skin" src="http://127.0.0.1:8887/BaseDomination_2.png"></div><div class="container_skin_buttons"><img class="first_logo_skin" src="http://127.0.0.1:8887/googleplay.png"><img class="second_logo_skin" src="http://127.0.0.1:8887/appstore.png"></div></div>');
+$('<div class="skin_main"><div class="container_skin"><a href="#"><img class="master_skin" src="http://127.0.0.1:8887/BaseDomination_2.png"/></a></div><div class="container_skin_buttons"><a href="#"><img class="first_logo_skin" src="http://127.0.0.1:8887/googleplay.png"/></a><a href="#"><img class="second_logo_skin" src="http://127.0.0.1:8887/appstore.png"/></a></div></div>').insertBefore('.header-wrapper-bottom');
