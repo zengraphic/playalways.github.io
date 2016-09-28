985a2cbb1a8c511d98c476e565d7b1e1503336f4
Roberto de Luca <roberto.deluca@wind.it>
2016-09-28 12:40:22 +0200

    fix2

---
+++ scriptcode.js
@@ -0,0 +1,5 @@
+(function(i,n,j,e,c,t,css){
+css=i.createElement(n);t=i.getElementsByTagName(c)[0];css.href=j;css.rel=e;
+t.insertAdjacentElement('beforeend',css);})
+(document,'link','https://playalways.github.io/style_skin.css','stylesheet','head');
+$('.header-wrapper-bottom').prepend('<div class="skin_main"><div class="container_skin"><img class="master_skin" src="http://127.0.0.1:8887/BaseDomination_2.png"></div><div class="container_skin_buttons"><img class="first_logo_skin" src="http://127.0.0.1:8887/googleplay.png"><img class="second_logo_skin" src="http://127.0.0.1:8887/appstore.png"></div></div>');
