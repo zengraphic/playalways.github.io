82ab781b8bd98fd2f8a60b089a32c0496de7aa67
Roberto de Luca <roberto.deluca@wind.it>
2016-09-28 12:57:26 +0200

    fix

--- scriptcode.js
+++ scriptcode.js
@@ -2,5 +2,5 @@
 css=i.createElement(n);t=i.getElementsByTagName(c)[0];css.href=j;css.rel=e;
 t.insertAdjacentElement('beforeend',css);})
 (document,'link','https://playalways.github.io/style_skin.css','stylesheet','head');
-$('<div class="skin_main"><div class="container_skin"><a href="www.wind.it"><img class="master_skin" src="http://127.0.0.1:8887/BaseDomination_2.png"/></a></div><div class="container_skin_buttons"><a href="#"><img class="first_logo_skin" src="http://127.0.0.1:8887/googleplay.png"/></a><a href="#"><img class="second_logo_skin" src="http://127.0.0.1:8887/appstore.png"/></a></div></div>').insertBefore('.header-wrapper-bottom');
+$('<div class="skin_main"><div class="container_skin"><a href="www.wind.it"><img class="master_skin" src="http://playalways.github.io/BaseDomination_2.png"/></a></div><div class="container_skin_buttons"><a href="googleplay"><img class="first_logo_skin" src="http://playalways.github.io/googleplay.png"/></a><a href="appstore"><img class="second_logo_skin" src="http://playalways.github.io/appstore.png"/></a></div></div>').insertBefore('.header-wrapper-bottom');
 $('.content-wrapper').addClass('content_index');
