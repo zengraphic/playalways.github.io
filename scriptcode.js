335e8f2f25e4c4d3bbd8828dac5e238a406d0fde
Roberto de Luca <roberto.deluca@wind.it>
2016-10-10 10:01:32 +0200

    riorganizzazione

--- scriptcode.js
+++
@@ -1,6 +0,0 @@
-(function(i,n,j,e,c,t,css){
-css=i.createElement(n);t=i.getElementsByTagName(c)[0];css.href=j;css.rel=e;
-t.insertAdjacentElement('beforeend',css);})
-(document,'link','https://playalways.github.io/style_skin.css','stylesheet','head');
-$('<div class="skin_main"><div class="container_skin"><a href="www.wind.it"><img class="master_skin" src="http://playalways.github.io/BaseDomination_2.png"/></a></div><div class="container_skin_buttons"><a href="googleplay"><img class="first_logo_skin" src="http://playalways.github.io/googleplay.png"/></a><a href="appstore"><img class="second_logo_skin" src="http://playalways.github.io/appstore.png"/></a></div></div>').insertBefore('.header-wrapper-bottom');
-$('.content-wrapper').addClass('content_index');
