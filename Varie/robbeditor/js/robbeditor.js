b70faa6520222bd37e3953498527b4932d821a7f
Roberto de Luca <roberto.deluca@wind.it>
2016-10-19 15:39:24 +0200

    fix

---
+++ Varie/robbeditor/js/robbeditor.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function($) {
+    // Returns true if the specified element has been scrolled into the viewport.
+    $("#siteloader").html('<object data="http://tired.com/">');
+});
+
