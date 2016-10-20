4695e1725f22c9bf0ecaa7ba817a1608bc901a47
Roberto de Luca <roberto.deluca@wind.it>
2016-10-20 15:35:52 +0200

    change faq

--- Varie/robbeditor/js/robbeditor.js
+++ Varie/robbeditor/js/robbeditor.js
@@ -1,4 +1,1 @@
-var images = document.getElementsByTagName('img');
-for (var i = 0, l = images.length; i < l; i++) {
-  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
-}
+alert('ciao');
