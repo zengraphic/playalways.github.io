c7723564cd95d337d1b5ef01917f0df92b0ce66d
Ilaria Picariello <ilaria.picariello@altran.com>
2016-11-18 12:26:36 +0100

    Add Fb tabs directory

---
+++ tabs-FB/supporto/config.php
@@ -0,0 +1,6 @@
+<?php 
+$pageTitle          = 'Wind Supporto';
+$analyticsAccount   = 'UA-26021590-1';
+$fbAppId            = '294566180591139';
+$postfix			= 'utm_source=wind&utm_medium=facebook&utm_content=ss&utm_campaign=Internet';
+?>
---
+++ tabs-FB/supporto/footertab.windit.php
@@ -0,0 +1,14 @@
+<div class="block" id="block4">
+  <div class="holder">
+	  <div class="inner">
+		<h3>Visita il nostro sito!</h3>
+		<p>Vai sul sito e scopri tutte le offerte per mobile, internet, fisso e molto altro ancora.</p>
+		<!-- button holder -->
+		<div class="btn-list">
+			<!-- btn01 -->
+			<li><a href="http://www.wind.it?utm_source=wind&utm_medium=facebook&utm_content=ss&utm_campaign=Internet" target="_blank" class="btn01">VISITA WIND.IT</a></li>
+			<li><a href="http://www.infostrada.it?utm_source=wind&utm_medium=facebook&utm_content=ss&utm_campaign=Internet" target="_blank" class="btn01">VISITA INFOSTRADA.IT</a></li>
+		</div>
+	</div>
+	</div>
+</div>
