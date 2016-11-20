28586c8bd04b6150dbf4508a87875d610656ed24
Simone Irato <Simone Irato>
2016-11-20 16:19:56 +0100

    fix booking2

--- canvas2/booking/js/booking2.js
+++ canvas2/booking/js/booking2.js
@@ -1,27 +1,12 @@
-jQuery(function($) {
-
-    $(document).ready(function() {
-        tabSwitch();
+jQuery(document).ready(function($) {
+    $('.offer_tab_nav_link a').click(function(event) {
+        event.preventDefault();
+        var tab = $(this).parent();
+        var tabContent = $($(this).attr('href'));
+        if (tab.not('.active')) {
+            $('.offer_tab_nav_link.active,.offer_tab_content .offer.active').removeClass('active');
+            tab.addClass('active');
+            tabContent.addClass('active');
+        }
     });
-    $(window).resize(function() {
-        tabSwitch();
-    });
-
-    function tabSwitch() {
-
-        $('.offer_tab_nav_link a').click(function(event) {
-
-            event.preventDefault();
-            link = $(this);
-            tab = $(this).parent();
-            tabContent = $(link.attr('href'));
-            if (tab.not('.active')) {
-                $('.offer_tab_nav_link.active').removeClass('active');
-                $('.offer_tab_content .offer.active').removeClass('active');
-                tab.addClass('active');
-                tabContent.addClass('active');
-            }
-
-        });
-    }
 });
