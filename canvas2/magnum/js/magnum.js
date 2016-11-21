cfa448ce18b12af845e43ec2b1ddcfe868c797ff
Reptoconcepts <reptoconcepts@gmail.com>
2016-11-21 20:46:55 +0100

    fix magnum.js

--- canvas2/magnum/js/magnum.js
+++ canvas2/magnum/js/magnum.js
@@ -1,9 +1,17 @@
 jQuery(function($) {
 
     function GetURLParameter(sParam) {
-        var sPageURL = window.location.hash;
-        var sPageHash = sPageURL.substr(sPageURL.indexOf('#'), (sPageURL.indexOf('?') || sPageURL.length-1))
-        var sURLVariables = sPageHash.split('&');
+        var sPageHash = window.location.hash;
+        var urlMark = sPageURL.indexOf('?');
+        var hashMark = sPageURL.indexOf('#');
+        var hashPart;
+        if (hashMark < urlMark) {
+            hashPart = sPageHash.substr(hashMark, urlMark);
+        } else {
+            hashPart = sPageHash.substr(hashMark, sPageURL.length - 1);
+        }
+        var sURLVariables = hashPart.split('&');
+
         for (var i = 0; i < sURLVariables.length; i++) {
             var sParameterName = sURLVariables[i].split('=');
             if (sParameterName[0] == sParam) {
