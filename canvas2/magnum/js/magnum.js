bb96155e9030f2313573df9ef614602b2659c600
Simone Irato <Simone Irato>
2016-11-21 17:22:00 +0100

    fix magnum

---
+++ canvas2/magnum/js/magnum.js
@@ -0,0 +1,28 @@
+jQuery(function($) {
+
+    function GetURLParameter(sParam) {
+        var sPageURL = window.location.search.substring(1);
+        var sURLVariables = sPageURL.split('&');
+        for (var i = 0; i < sURLVariables.length; i++) {
+            var sParameterName = sURLVariables[i].split('=');
+            if (sParameterName[0] == sParam) {
+                return sParameterName[1];
+            }
+        }
+    }
+    $(document).ready(function() {
+        if (GetURLParameter('phones') == 'samsung') {
+            $('.landing_magnum__showcase__item')
+                .css({
+                    'width': '35%'
+                });
+            $('.landing_magnum__showcase__item:nth-child(3)')
+                .css({
+                    'display': 'none'
+                });
+        }
+    });
+
+
+
+});
