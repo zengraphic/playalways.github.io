4bb3bc25f98effe2f5b1a874e3d482600c35916b
Simone Irato <Simone Irato>
2016-11-21 18:12:10 +0100

    magnum parameters hash fix

--- canvas2/magnum/js/magnum.js
+++ canvas2/magnum/js/magnum.js
@@ -1,8 +1,10 @@
 jQuery(function($) {
 
     function GetURLParameter(sParam) {
-        var sPageURL = window.location.search.substring(1);
+        var sPageURL = window.location.hash;
         var sURLVariables = sPageURL.split('&');
+        console.log(sPageURL);
+        console.log(sURLVariables);
         for (var i = 0; i < sURLVariables.length; i++) {
             var sParameterName = sURLVariables[i].split('=');
             if (sParameterName[0] == sParam) {
@@ -11,7 +13,7 @@ jQuery(function($) {
         }
     }
     $(document).ready(function() {
-        if (GetURLParameter('phones') == 'samsung') {
+        if (GetURLParameter('#phones') == 'samsung') {
             $('.landing_magnum__showcase__item')
                 .css({
                     'width': '35%'
