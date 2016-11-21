b483b57b966aec377f02877664ac4ca414325bed
Reptoconcepts <reptoconcepts@gmail.com>
2016-11-21 20:30:05 +0100

    fix magnum.js

--- canvas2/magnum/js/magnum.js
+++ canvas2/magnum/js/magnum.js
@@ -2,9 +2,8 @@ jQuery(function($) {
 
     function GetURLParameter(sParam) {
         var sPageURL = window.location.hash;
-        var sURLVariables = sPageURL.split('&');
-        console.log(sPageURL);
-        console.log(sURLVariables);
+        var sPageHash = sPageURL.substr(0, sPageURL.indexOf('?'))
+        var sURLVariables = sPageHash.split('&');
         for (var i = 0; i < sURLVariables.length; i++) {
             var sParameterName = sURLVariables[i].split('=');
             if (sParameterName[0] == sParam) {
@@ -14,10 +13,7 @@ jQuery(function($) {
     }
     $(document).ready(function() {
         if (GetURLParameter('#phones') == 'samsung') {
-            $('.landing_magnum__showcase__item')
-                .css({
-                    'width': '35%'
-                });
+            $('.landing_magnum__showcase__item');
             $('.landing_magnum__showcase__item:nth-child(3)')
                 .css({
                     'display': 'none'
