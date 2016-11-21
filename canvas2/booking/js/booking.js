4f57c5121a0946e554e76a289a0725b11cae8eb8
Simone Irato <Simone Irato>
2016-11-21 12:28:31 +0100

    bug fixes and package booking

--- canvas2/booking/js/booking.js
+++ canvas2/booking/js/booking.js
@@ -1,48 +1,12 @@
 jQuery(document).ready(function($) {
-
-    $(".offer_tab_nav_link a").click(function(event) {
+    $('.offer_tab_nav_link a').click(function(event) {
         event.preventDefault();
-
-        var tab = $(this).attr("href");
-        $(this).parent().addClass("active");
-        $(this).parent().siblings().removeClass("active");
-        if (tab == '#offer1') {
-            $('#offer1').show();
-            $('#offer2').hide();
-        } else {
-            $('#offer2').show();
-            $('#offer1').hide();
+        var tab = $(this).parent();
+        var tabContent = $($(this).attr('href'));
+        if (tab.not('.active')) {
+            $('.offer_tab_nav_link.active,.offer_tab_content .offer.active').removeClass('active');
+            tab.addClass('active');
+            tabContent.addClass('active');
         }
-
-    });
-
-  // This will execute whenever the window is resized
-  var ww = $(window).resize().width();// New width
-    if (ww <= 640) {
-        $('.info_offer__details--right')
-            .insertAfter('.offer_tab_content')
-            .css({
-                'width':'100%',
-                'margin-top':'20px',
-                'margin-bottom':'20px',
-                'text-align':'center',
-            });
-    }
-
-
-    $(".btn_3").click(function() {
-        $('#offer1').hide();
-        $('.btn_1').hide();
-        $('.btn_2').addClass('active').show();
-        $('.btn_4').show();
-        $('.btn_3').hide();
-    });
-
-    $(".btn_4").click(function() {
-        $(this).addClass('active');
-        $('.btn_2').hide();
-        $('.btn_3').show();
-        $(this).hide();
-        $('.btn_1').show();
     });
 });
