d92a44a331fdca362afd18a3fffe845ff916ae17
Simone Irato <Simone Irato>
2016-12-02 12:22:11 +0100

    fix canvass2

---
+++ canvas2/magnum_dicembre_2/js/magnum.js
@@ -0,0 +1,35 @@
+jQuery(function($) {
+
+    function GetURLParameter(sParam) {
+        var sPageHash = window.location.hash;
+        var urlMark = sPageHash.indexOf('?')-1;
+        var hashMark = sPageHash.indexOf('#')+1;
+        var hashPart;
+        if (hashMark < urlMark) {
+            hashPart = sPageHash.substr(hashMark, urlMark);
+        } else {
+            hashPart = sPageHash.substr(hashMark, sPageHash.length - 1);
+        }
+        var sHashVariables = hashPart.split('&');
+        console.log(sHashVariables);
+
+        for (var i = 0; i < sHashVariables.length; i++) {
+            var sParameterName = sHashVariables[i].split('=');
+            if (sParameterName[0] == sParam) {
+                return sParameterName[1];
+            }
+        }
+    }
+    $(document).ready(function() {
+        if (GetURLParameter('phones') == 'samsung') {
+            $('.landing_magnum__showcase__item');
+            $('.landing_magnum__showcase__item:nth-child(3)')
+                .css({
+                    'display': 'none'
+                });
+        }
+    });
+
+
+
+});
