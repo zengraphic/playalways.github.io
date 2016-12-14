9ba567b27e451e990fcf04017d4b8f7cd7981a66
zengraphic <info@zengraphic.it>
2016-12-14 12:32:30 +0100

    sms

--- Varie/provasms/js/sms.js
+++ Varie/provasms/js/sms.js
@@ -2,9 +2,7 @@ jQuery(document).ready(function($) {
     function sms() {
         var userAgent = navigator.userAgent || navigator.vendor || window.opera;
         // Windows Phone must come first because its UA also contains "Android"
-        if (/windows phone/i.test(userAgent)) {
-            //WP
-        } else if (/android/i.test(userAgent)) {
+     if (/android/i.test(userAgent)) {
             //Android
             $('.iphone').css('display', 'none');
         } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
@@ -12,4 +10,6 @@ jQuery(document).ready(function($) {
             $('.android').css('display', 'none');
         }
     }
+
+    sms();
 });
