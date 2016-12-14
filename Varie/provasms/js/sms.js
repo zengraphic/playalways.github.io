d48b0bb353676084dabb4e9a157471024350b51f
zengraphic <info@zengraphic.it>
2016-12-14 14:55:36 +0100

    ticketing sms

--- Varie/provasms/js/sms.js
+++ Varie/provasms/js/sms.js
@@ -5,10 +5,12 @@ jQuery(function($) {
         // Windows Phone must come first because its UA also contains "Android"
      if (/android/i.test(userAgent)) {
             //Android
+            $('.android').css('display', 'block');
             $('.iphone').css('display', 'none');
             $('.smsDesk').css('display', 'none');
         } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
             //iOS
+            $('.iphone').css('display', 'block');
             $('.android').css('display', 'none');
             $('.smsDesk').css('display', 'none');
         }
