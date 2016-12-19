981f01bc368cb0ffd0d6f194ea629a1e6b1111ef
Roberto de Luca <roberto.deluca@wind.it>
2016-12-19 17:40:15 +0100

    fix

--- Varie/robbeditor/js/editor.js
+++ Varie/robbeditor/js/editor.js
@@ -1,37 +1,55 @@
 jQuery(document).ready(function($) {
 
+    //stilizza i tag
+    //toglie il binding a tutti i tasti di edit
+    function unbindAll() {
+        $('.edit_tool').unbind('click');
+    }
+
+    //splitta dopo il "-" e aggiunge area. (area-hero)
     function getBaseClass(elem) {
         var classTotal = $(elem).attr('class');
         var classTotal = classTotal.split('-')[1];
-        var baseTarget ='area-' + classTotal;
-        return(baseTarget);
+        var baseTarget = 'area-' + classTotal;
+        return (baseTarget);
     }
 
+    //prende la classe base dell'elemento, splitta dopo il "-" e aggiunge target_ (target_area-hero)    
     function getEndClass(elem) {
         var endTarget = getBaseClass(elem);
         var endTarget = 'target_' + endTarget;
-        return(endTarget);
-    }    
-
-    function removeString(elem){
-        var stringtoRemove =  '<div class="edit_tool '+getBaseClass($(elem))+'"></div>';
-        console.log(stringtoRemove);
-        return(stringtoRemove);
+        return (endTarget);
     }
 
-    $('.edit_tool').click(function() {
-        var baseItem = "." + getBaseClass($(this));
-        var endItem = "." + getEndClass($(this));
-        $(endItem).slideToggle();
-        toEdit = $(this).parents('.anchor_tool').html();
-        //toEdit = toEdit.replace(removeString($(this)),'');
-        $(endItem).val(toEdit);        
-    });
+    //prende la classe base dell'elemento, splitta dopo il "-" e aggiunge anchor_ (anchor_area-hero) 
+    function getAnchor(elem) {
+        var endTarget = getBaseClass(elem);
+        var endTarget = 'anchor_' + endTarget;
+        return (endTarget);
+    }
+
+    //binda il click al tool
+    function bindEdit(element) {
+        $(element).on("click", function(event) {
+            var baseItem = "." + getBaseClass($(this));
+            var endItem = "." + getEndClass($(this));
+            var anchorItem = "." + getAnchor($(this));
+            //console.log("base: " + baseItem + " end: " + endItem);
+            $(endItem).slideToggle();
+            toEdit = $(anchorItem).html();
+            console.log(anchorItem);
+            //toEdit = toEdit.replace(removeString($(this)),'');
+            $(endItem).text(toEdit);
+        });
+    };
 
     $('.area_tool').bind("keyup", function() {
-        var classTotal = "." + $(this).attr('class').split('target_')[1];
-        console.log(classTotal);
-        var newHtml = $(this).val();
-        $(classTotal).parents('.anchor_tool').html(newHtml);        
+        var anchorItem = "." + getAnchor($(this));
+        var newHtml = $(this).text();
+        unbindAll();
+        $(anchorItem).html(newHtml);
+        bindEdit('.edit_tool');
     });
+
+    bindEdit('.edit_tool');
 });
