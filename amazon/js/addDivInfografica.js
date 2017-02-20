1cf8395a41b447b7041e8b46adb5bac728b67e44
Roberto de Luca <roberto.deluca@wind.it>
2017-02-20 16:45:02 +0100

    canvas + altro

---
+++ amazon/js/addDivInfografica.js
@@ -0,0 +1,7 @@
+(function(){   
+    var countup = this;
+    var newNode = document.createElement('div');
+    newNode.className = 'infografica_container';
+    newNode.innerHTML = '<div class="infografica_img_container"><img class="infographic_image" src="https://playalways.github.io/amazon_infografica.jpg"/></div>';
+    document.getElementById('vl-text-comefunziona').appendChild(newNode);           
+})();
