4ac58b4db59fbf0fb2ed37c30527409f7953df19
Roberto de Luca <roberto.deluca@wind.it>
2017-03-13 13:02:09 +0100

    add wd

--- wind-digital/chiacchiere/js/calendar_wd_read.js
+++ wind-digital/chiacchiere/js/calendar_wd_read.js
@@ -1,27 +1,72 @@
-jQuery(document).ready(function($) {
-    // ================== CARICAMENTO DATI ===================
-    var url = 'https://script.googleusercontent.com/macros/echo?user_content_key=Vl1tOe6HCcc9Z-QRKROK_4_yKkyhX7bs55OR0G5Mv3AuHFmtr1-UgAh10oUmP-p5tDXswrOW6c6vvaZ9-nfKXMrKY-WPJJGIOJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMWojr9NvTBuBLhyHCd5hHa1ZsYSbt7G4nMhEEDL32U4DxjO7V7yvmJPXJTBuCiTGh3rUPjpYM_V0PJJG7TIaKpwiphq7z5M5_x__2Yg-t8t4__Q9xYWJLseODltSRTQGrK2sUIYrbeoJIchWhiPvIYDQJv8DXWa1h6YynRodI14iC1FYEJlGtPA&lib=MbpKbbfePtAVndrs259dhPT7ROjQYJ8yx';
-    $.getJSON(url, function(data) {
-        $.each(data, function(index, element) {
-            console.log(data);
-            var timeStamp = data[index][0].Timestamp;
-            var type = data[index][0].Type;
-            var title = data[index][0].Title;
-            var author = data[index][0].Author;
-            var description = data[index][0].Description;
-            var url = data[index][0].url;
-            var note = data[index][0].Note;
-            $('.calendar__container')
-                .append(
-                    '<div>' + timeStamp + '</div>' +
-                    '<div>' + type + '</div>' +
-                    '<div>' + title + '</div>' +
-                    '<div>' + author + '</div>' +
-                    '<div>' + description + '</div>' +
-                    '<div>' + url + '</div>' +
-                    '<div>' + note + '</div>'
-                );
-        });
-    });
-    /*https://script.google.com/macros/s/AKfycbygukdW3tt8sCPcFDlkMnMuNu9bH5fpt7bKV50p2bM/exec?id=1yhZTKp_VeR0EaaitSXnax2JMdBusqe3lZf8OQ9YuKmY&sheet=Form Responses 1*/
-});
+  var spData = null;
+
+  function doData(json) {
+      spData = json.feed.entry;
+  }
+
+  function checklink(val) {
+      if (val && !val.match(/^.+:\/\/.*/)) {
+          return (true);
+      } else {
+          return (false);
+      }
+  }
+
+  function drawCell(tr, val) {
+      if (checklink(val)) {
+          var td = $("<td class='wd_line'/>");
+      } else {
+          var value = val;
+          var td = $("<td class='wd_line link_line'/>");
+      }
+      tr.append(td);
+      td.append(val);
+      return td;
+  }
+
+  function drawRow(table, rowData) {
+      if (rowData == null) return null;
+      if (rowData.length == 0) return null;
+      var tr = $("<tr class='wd_row'/>");
+      table.append(tr);
+      for (var c = 0; c < rowData.length; c++) {
+          drawCell(tr, rowData[c]);
+      }
+      return tr;
+  }
+
+  function drawTable(parent) {
+      var table = $("<table/>");
+      parent.append(table);
+      return table;
+  }
+
+  function readData(parent) {
+      var data = spData;
+      var table = drawTable(parent);
+      var rowData = [];
+
+      for (var r = 0; r < data.length; r++) {
+          var cell = data[r]["gs$cell"];
+          var val = cell["$t"];
+          if (cell.col == 1) {
+              drawRow(table, rowData);
+              rowData = [];
+          }
+          rowData.push(val);
+      }
+      drawRow(table, rowData);
+  }
+
+  jQuery(document).ready(function($) {
+      readData($("#data"));
+
+      $('.link_line').click(function() {
+          linkToGo = $(this).text();
+          window.open(linkToGo);
+          return false;
+      });
+  });
+  //https://spreadsheets.google.com/feeds/cells/1yhZTKp_VeR0EaaitSXnax2JMdBusqe3lZf8OQ9YuKmY/1/public/values?alt=json-in-script
+
+  //https://spreadsheets.google.com/feeds/cells/1yhZTKp_VeR0EaaitSXnax2JMdBusqe3lZf8OQ9YuKmY/1/public/values?alt=json-in-script&callback=doData
