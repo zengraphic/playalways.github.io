81918a9121c1d6882779eb09e821c28fdb9f0b59
sambrosetti <soniaambrosetti@hotmail.com>
2017-03-22 12:34:10 +0100

    Landing Wind Magnum 5 - 10 - 30 GB

--- canvass/magnum_marzo_2017/js/booking.js
+++ canvass/magnum_marzo_2017/js/booking.js
@@ -1,12 +1,49 @@
 jQuery(document).ready(function($) {
-    $('.offer_tab_nav_link a').click(function(event) {
-        event.preventDefault();
-        var tab = $(this).parent();
-        var tabContent = $($(this).attr('href'));
-        if (tab.not('.active')) {
-            $('.offer_tab_nav_link.active,.offer_tab_content .offer.active').removeClass('active');
-            tab.addClass('active');
-            tabContent.addClass('active');
-        }
+    // $('.magnum_nav_link a').click(function(event) {
+    //     event.preventDefault();
+    //     var tab = $(this).parent(); //mshnum_nav_link
+    //     var tabContent = $($(this).attr('href')); //href dell'a dove ho cliccato (#offer2)
+    //     if (tab.not('.active')) {
+    //         $('.active').removeClass('active');
+    //         tab.addClass('active');
+    //         tabContent.addClass('active');
+    //     }
+    // });
+    $('.cta_btn_1').click(function(e) {
+        e.preventDefault();
+        $('.btn_1').addClass('active');
+        $('.btn_2').removeClass('active');
+        $('.btn_3').removeClass('active');
+        $('.offer1').addClass('active');
+        $('.offer2').removeClass('active');
+        $('.offer3').removeClass('active');
+        $('.hero_1').addClass('active');
+        $('.hero_2').removeClass('active');
+        $('.hero_3').removeClass('active');
+
+    });
+    $('.cta_btn_2').click(function(e) {
+e.preventDefault();
+        $('.btn_2').addClass('active');
+        $('.btn_1').removeClass('active');
+        $('.btn_3').removeClass('active');
+        $('.offer2').addClass('active');
+        $('.offer1').removeClass('active');
+        $('.offer3').removeClass('active');
+        $('.hero_2').addClass('active');
+        $('.hero_1').removeClass('active');
+        $('.hero_3').removeClass('active');
+    });
+    $('.cta_btn_3').click(function(e) {
+        e.preventDefault();
+        $('.btn_3').addClass('active');
+        $('.btn_1').removeClass('active');
+        $('.btn_2').removeClass('active');
+        $('.offer3').addClass('active');
+        $('.offer1').removeClass('active');
+        $('.offer2').removeClass('active');
+        $('.hero_3').addClass('active');
+        $('.hero_1').removeClass('active');
+        $('.hero_2').removeClass('active');
     });
 });
