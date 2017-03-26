4e37e70f661f96271ad4cad18ccf0de830e09bf6
SimoneIrato <simone.irato@serasoft.it>
2017-03-26 16:21:07 +0200

    WindTre Header

--- windtre-header/js/windtre-header.js
+++ windtre-header/js/windtre-header.js
@@ -10,11 +10,10 @@ jQuery(function($) {
         autohidemode: false,
         zindex: 900
     };
-
     var primaryNavConfig = {
         arrows: false,
         infinite: false,
-        slidesToShow: 6,
+        slidesToShow: 1,
         prevArrow: '<a class="prev">&lt;</a>',
         nextArrow: '<a class="next">&gt;</a>',
         variableWidth: true,
@@ -55,10 +54,51 @@ jQuery(function($) {
             $scrollable
                 .niceScroll(scrollConfig);
 
-            $primaryNav
-                .slick(primaryNavConfig);
             $secondaryNav
                 .slick(secondaryNavConfig);
+
+
+            var sticky = new Waypoint.Sticky({
+                element: $('.windtre__primary-nav')
+            });
+
+            $primaryNav
+                .slick(primaryNavConfig);
+
+            $('.nav_link','#header')
+                .on('click', function(event) {
+                    event.preventDefault();
+                    var logo = $('.windtre__primary-nav__logo').outerWidth();
+                    var lang = $('.windtre__primary-nav__language').outerWidth();
+                    var theParent =$(this).parent().parent().parent();
+                    theParent
+                        .addClass('active');
+                    var actives = $('#header').find('.active');
+                    var activesW;
+                    actives
+                        .each(function() {
+                            activesW += $(this).outerWidth();
+                        });
+                    theParent
+                        .parents('.slick-initialized')
+                        .slick('unslick');
+                    theParent
+                        .siblings()
+                        .hide();
+
+                    theParent
+                        .children('.nav_link--sub')
+                        .eq(0)
+                        .show()
+                        .css({
+                            'width': 'calc(100% -'+logo+'px-'+lang+'px-'+activesW+')'
+                        })
+                        .slick(primaryNavConfig);
+
+
+
+
+                });
         });
 
 
