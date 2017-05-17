c3127f8a2e808f79c188aa3eb8b1c0e18d6e6732
Roberto De Luca <roberto.deluca@windtre.it>
2017-05-17 12:09:57 +0200

    update landing

--- fileadmin/js-landing/landing.js
+++ fileadmin/js-landing/landing.js
@@ -1,4 +1,14 @@
 jQuery(document).ready(function($) {
+    $('body').addClass('landing');
+
+    $('.button_shoulder_login').click(function() {
+        r$.magnificPopup.open({
+            items: {
+                src: $('#login')
+            }
+        });
+    });
+
     $('body').on("click", ".landing__hero__tabs--button", function() {
         var tabNumber = $(this).data('tabn');
         var element = $(this);
@@ -18,38 +28,38 @@ jQuery(document).ready(function($) {
 
 });
 
-    function loadSlick() {
-        $('.slider__device').slick({
-            dots: true,
-            arrows: true,
-            infinite: false,
-            speed: 300,
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            nextArrow: "<img class='slider__navigation right__arrow' src='/fileadmin/img-landing/icons-interface/slider_arrow_right.png'>",
-            prevArrow: "<img class='slider__navigation left__arrow' src='/fileadmin/img-landing/icons-interface/slider_arrow_left.png'>",
-            dotsClass: 'slider__dots',
-            customPaging: function(slider, i) {
-                var thumb = $(slider.$slides[i]).data('thumb');
-                return '<div class="slider__single"></div>';
+function loadSlick() {
+    $('.slider__device').slick({
+        dots: true,
+        arrows: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        nextArrow: "<img class='slider__navigation right__arrow' src='/fileadmin/img-landing/icons-interface/slider_arrow_right.png'>",
+        prevArrow: "<img class='slider__navigation left__arrow' src='/fileadmin/img-landing/icons-interface/slider_arrow_left.png'>",
+        dotsClass: 'slider__dots',
+        customPaging: function(slider, i) {
+            var thumb = $(slider.$slides[i]).data('thumb');
+            return '<div class="slider__single"></div>';
+        },
+        responsive: [{
+                breakpoint: 980,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    dots: true
+                }
+            }, {
+                breakpoint: 670,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
             },
-            responsive: [{
-                    breakpoint: 980,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2,
-                        dots: true
-                    }
-                }, {
-                    breakpoint: 670,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                    }
-                },
-                // You can unslick at a given breakpoint now by adding:
-                // settings: "unslick"
-                // instead of a settings object
-            ]
-        });
-    }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+}
