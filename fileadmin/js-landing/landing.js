6b6d9fe0b82e378bb33b16ec04ac1b4fb7e3de19
Roberto De Luca <roberto.deluca@windtre.it>
2017-05-23 16:46:45 +0200

    update

--- fileadmin/js-landing/landing.js
+++ fileadmin/js-landing/landing.js
@@ -1,4 +1,3 @@
-jQuery(document).ready(function($) {
     function loadSlickMagnum() {
         $('.slider__device').slick({
             dots: true,
@@ -35,49 +34,11 @@ jQuery(document).ready(function($) {
         });
     }
 
-    function loadSlickTwoDevices() {
-        $('.slider__device_twoDevices').slick({
-            dots: true,
-            arrows: true,
-            infinite: false,
-            speed: 300,
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            nextArrow: "<img class='slider__navigation right__arrow' src='/fileadmin/img-landing/icons-interface/slider_arrow_right.png'>",
-            prevArrow: "<img class='slider__navigation left__arrow' src='/fileadmin/img-landing/icons-interface/slider_arrow_left.png'>",
-            dotsClass: 'slider__dots',
-            customPaging: function(slider, i) {
-                var thumb = $(slider.$slides[i]).data('thumb');
-                return '<div class="slider__single"></div>';
-            },
-            responsive: [{
-                    breakpoint: 980,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2,
-                        dots: true
-                    }
-                }, {
-                    breakpoint: 670,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                    }
-                },
-                // You can unslick at a given breakpoint now by adding:
-                // settings: "unslick"
-                // instead of a settings object
-            ]
-        });
-    }
+jQuery(document).ready(function($) {
 
     if ($('.slider__device')) {
         loadSlickMagnum();
     }
-    if ($('.slider__device_twoDevices')) {
-        loadSlickTwoDevices();
-    }
-
     $('body').addClass('landing');
 
     $('.button_shoulder_login').click(function() {
