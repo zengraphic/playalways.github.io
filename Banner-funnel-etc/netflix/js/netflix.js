371f6fb2f5e666cfa111b492389ee2514e5d1856
Roberto De Luca <roberto.deluca@windtre.it>
2017-06-08 15:38:21 +0200

    update

--- Banner-funnel-etc/netflix/js/netflix.js
+++ Banner-funnel-etc/netflix/js/netflix.js
@@ -1,54 +1,65 @@
 	jQuery(document).ready(function($) {
-	    function glitch() {
-	        if ($('.hero_full_container').hasClass('upside_down')) {
-	            upsideDown();
-	            setTimeout(upsideDown, 500);
-	        } else {
-	            reverseUpside();
-	        }
-	    }
 
-	    function pollen() {
-	        $('body').append('<div class="pollen"></div>');
-	    }
+	            var stackN = 0;
 
-	    function upsideDown() {
-	        setTimeout(function() {
-	            $('.hero_full_container, .offer_full_container').addClass('upside_down');
-	        }, 500);
-	        reverseUpside();
-	    }
+	            function glitch() {
+	            	stackN = 0;
+	                if ($('.hero_full_container').hasClass('upside_down')) {
+	                    upsideDown();
+	                    setTimeout(upsideDown, 500);
+	                } else {
+	                    reverseUpside();
+	                }
+	            }
 
-	    function reverseUpside() {
-	        setTimeout(function() {
-	            $('.hero_full_container, .offer_full_container').removeClass('upside_down');
-	        }, 500);
-	        upsideDown();
-	    }
+	            function pollen() {
+	                $('body').append('<div class="pollen"></div>');
+	            }
 
-	    function overlayFadeInOut() {
-	        $('body').append('<div class="netflix_overlay"></div>');
-	        setInterval(glitch,2500);
-	    }
+	            function upsideDown() {
+	                if (stackN < 10) {
+	                    stackN = stackN + 1;
+	                    setTimeout(function() {
+	                        $('.hero_full_container, .offer_full_container').addClass('upside_down');
+	                    }, 500);
+	                    reverseUpside();
+	                }
+	            }
 
-	    var kkeys = [],
-	        konami = "38,38,40,40,37,39,37,39,66,65",
-	        strange = "83,84,82,65,78,71,69",
-	        netflix = "78,69,84,70,76,73,88",
-	        sottosopra = "83,79,84,84,79,83,79,80,82,65";
+	            function reverseUpside() {
+	                function upsideDown() {
+	                    if (stackN < 10) {
+	                        stackN = stackN + 1;
+	                        setTimeout(function() {
+	                            $('.hero_full_container, .offer_full_container').removeClass('upside_down');
+	                        }, 500);
+	                        upsideDown();
+	                    }
+	                }
+
+	                function overlayFadeInOut() {
+	                    $('body').append('<div class="netflix_overlay"></div>');
+	                    setInterval(glitch, 2500);
+	                }
+
+	                var kkeys = [],
+	                    konami = "38,38,40,40,37,39,37,39,66,65",
+	                    strange = "83,84,82,65,78,71,69",
+	                    netflix = "78,69,84,70,76,73,88",
+	                    sottosopra = "83,79,84,84,79,83,79,80,82,65";
 
 
-	    $(document).keydown(function(e) {
-	        kkeys.push(e.keyCode);
+	                $(document).keydown(function(e) {
+	                    kkeys.push(e.keyCode);
 
-	        if (kkeys.toString().indexOf(sottosopra) >= 0) {
+	                    if (kkeys.toString().indexOf(sottosopra) >= 0) {
 
-	            $(document).unbind('keydown', arguments.callee);
+	                        $(document).unbind('keydown', arguments.callee);
 
-	            // do something awesome
-	            pollen();
-	            overlayFadeInOut();
-	        }
+	                        // do something awesome
+	                        pollen();
+	                        overlayFadeInOut();
+	                    }
 
-	    });
-	});
+	                });
+	            });
