471a41beed27f7dcb933fd749b7b17d430ee033f
Roberto De Luca <roberto.deluca@windtre.it>
2017-07-14 16:35:32 +0200

    update

--- veon/newveon/js/veon.js
+++ veon/newveon/js/veon.js
@@ -24,15 +24,14 @@ jQuery(document).ready(function($) {
         } else if (/android/i.test(userAgent)) {
             //Android
             $('.appstore_button').css('display', 'none');
-            $('.standard_button').css('display','none');
+            $('.standard_button').css('display', 'none');
         } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
             //iOS
             $('.gplay_button').css('display', 'none');
-            $('.standard_button').css('display','none');            
-        }
-        else{
+            $('.standard_button').css('display', 'none');
+        } else {
             $('.gplay_button').css('display', 'none');
-            $('.appstore_button').css('display', 'none');            
+            $('.appstore_button').css('display', 'none');
         }
     }
 
@@ -61,9 +60,38 @@ jQuery(document).ready(function($) {
             }]
         });
     }
+
+    function checkWaypoint(element) {
+        var waypoint = new Waypoint({
+            element: document.getElementById(element),
+            handler: function(direction) {
+                console.log('Direction: ' + direction)
+                if (direction == 'down') {
+                    $(".button_veon").animate({
+                        bottom: "0"
+                    }, 1000, function() {
+                        // Animation complete.
+                    });
+                }
+            }
+        });
+    }
+
+    function initWaypoint() {
+        if ($(window).width() < 768) {
+            checkWaypoint('buttonWaypointMobile');
+        } else {
+            $(".button_veon").animate({
+                bottom: "0"
+            }, 1000, function() {
+                // Animation complete.
+            });
+        }
+    }
+
     $('.veon_slider_container').on('afterChange', function(event, slick, currentSlide) {
         var lastSlide = 4;
-        if($(window).width() > 768){
+        if ($(window).width() > 768) {
             lastSlide = 3;
         }
         if (currentSlide === lastSlide) {
@@ -80,4 +108,5 @@ jQuery(document).ready(function($) {
     })
     storeOS();
     initSliders();
+    initWaypoint();
 });
